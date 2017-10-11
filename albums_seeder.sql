6a4ddd883ad18f48d698c8d2cb63330c4003b6ed
Fernando <fernandoza@gmail.com>
2017-10-11 15:37:57 -0500

    Solution for the delete exercise.
    - Added the truncate statement in the seeder.
    - Created the delete statements in the file.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,7 @@
 Use codeup_test_db;
 
+TRUNCATE albums;
+
 INSERT INTO albums (`artist`,`name`,`release_date`,`sales`,`genre`) VALUES('Michael Jackson','Thriller',1982,46,'Pop, rock, R&B');
 INSERT INTO albums (`artist`,`name`,`release_date`,`sales`,`genre`) VALUES('AC/DC','Back in Black',1980,26.1,'Hard rock');
 INSERT INTO albums (`artist`,`name`,`release_date`,`sales`,`genre`) VALUES('Pink Floyd','The Dark Side of the Moon',1973,24.2,'Progressive rock');
---
+++ delete_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_db_test;
+
+DELETE FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums where genre = 'Disco';
+
+delete from albums where artist = 'Adele';
