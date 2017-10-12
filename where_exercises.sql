0565082c60f18c29307922390cbe2662e000bb55
Luis Montealegre <montealegreluis@gmail.com>
2017-10-12 10:59:59 -0500

    Complete the exercise for the where continued lesson

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,15 +6,47 @@ SELECT
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+SELECT
+  gender,
+  first_name,
+  last_name
+FROM employees
+WHERE (first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya')
+         AND gender = 'M';
+
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'e%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 
 SELECT
   hire_date,
   first_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT
+  hire_date,
+  first_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%-12-25';
 
 SELECT
   hire_date,
@@ -31,3 +63,8 @@ WHERE birth_date LIKE '%-12-25';
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
